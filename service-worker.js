ac0acef62231ecdc980d50d6ac4f88aefdff4bd4
carlostercenio-gif <carlostercenio@gmail.com>
2026-02-02 19:43:13 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,38 @@
-const CACHE_NAME = 'cocina-pop-client-v1';
+const CACHE_NAME = 'cocina-pop-client-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js'
+];
 
+// Instalación
 self.addEventListener('install', event => {
-  console.log('Service Worker instalado');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
+// Activación - borrar caché viejo
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Interceptar peticiones
 self.addEventListener('fetch', event => {
-  console.log('Fetch interceptado');
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
